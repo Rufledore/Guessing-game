9d40f65a7e48539acf2ceadec43232d41e9fb6c7
rufledore <ryfyldor@gmail.com>
2018-08-16 18:11:19 +0300

    guessinggame.sh is fully fucntional

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,26 @@
-numOfFiles=ls -al | grep "^-" | wc -l
-echo $numOfFiles
+#Finding the number of files in directory.
+numOfFiles=$(ls -al | grep "^-" | wc -l)
+
+isGuesRight=false
+
+echo "Gues the number of files in this directiry."
+
+while true;do
+	read userGues
+	if [[ $userGues -eq $numOfFiles ]]
+	then
+		echo "Congratulations! Your gues is right!"
+		break
+	else
+		if [[ $userGues -gt $numOfFiles ]]
+		then
+			echo "Your gues is greater than the right answer."
+			echo "Try one more time."
+		fi
+		if [[ $userGues -lt $numOfFiles ]]
+		then
+			echo "Your gues is lower than the right answer."
+			echo "Try one more time."
+		fi
+	fi
+done
